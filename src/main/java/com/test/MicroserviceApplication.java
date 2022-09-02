90c148b1e809e0496f3bdb028768dda8965cbb93
AMERICAS\otia <tushar.otia@hpe.com>
2022-09-02 17:42:19 -0500

    test

--- src/main/java/com/test/MicroserviceApplication.java
+++ src/main/java/com/test/MicroserviceApplication.java
@@ -1,5 +1,6 @@
 package com.test;
 
+import brave.baggage.CorrelationScopeDecorator;
 import ch.qos.logback.classic.PatternLayout;
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 import ch.qos.logback.core.ConsoleAppender;
@@ -8,6 +9,7 @@ import ch.qos.logback.core.rolling.SizeAndTimeBasedRollingPolicy;
 import com.test.connectors.kafka.KafkaReceiver;
 import com.test.connectors.kafka.KafkaSender;
 import com.test.controller.DefaultController;
+import org.springframework.beans.factory.Aware;
 import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.config.BeanPostProcessor;
@@ -16,9 +18,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.web.servlet.filter.OrderedRequestContextFilter;
+import org.springframework.cloud.sleuth.BaggageManager;
+import org.springframework.cloud.sleuth.CurrentTraceContext;
+import org.springframework.cloud.sleuth.TraceContext;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.cloud.sleuth.brave.bridge.BraveCurrentTraceContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.core.env.EnvironmentCapable;
+import org.springframework.data.mongodb.core.*;
+import org.springframework.data.mongodb.core.index.IndexOperationsProvider;
 import org.springframework.nativex.hint.*;
 import org.springframework.web.context.ConfigurableWebApplicationContext;
 import org.springframework.web.filter.RequestContextFilter;
@@ -28,28 +37,31 @@ import javax.servlet.ServletConfig;
 
 import java.io.Serializable;
 
-import static org.springframework.nativex.hint.TypeAccess.PUBLIC_METHODS;
-import static org.springframework.nativex.hint.TypeAccess.QUERY_PUBLIC_METHODS;
+import static org.springframework.nativex.hint.TypeAccess.*;
+import static org.springframework.nativex.hint.TypeAccess.PUBLIC_FIELDS;
 
 
 
-@JdkProxyHint(types= { BeanPostProcessor.class, BeanFactoryAware.class, BeanClassLoaderAware.class,
-        ApplicationContextAware.class, EnvironmentCapable.class, EnvironmentAware.class, Servlet.class, ServletConfig.class, Serializable.class})
 @NativeHint(
         types = {
-
-                @TypeHint(types = {BeanPostProcessor.class, BeanFactoryAware.class, BeanClassLoaderAware.class, ApplicationContextAware.class, EnvironmentCapable.class, EnvironmentAware.class, Servlet.class, ServletConfig.class, Serializable.class},
-                        access = {TypeAccess.PUBLIC_CONSTRUCTORS, PUBLIC_METHODS}),
                 @TypeHint(types = {ConsoleAppender.class, SizeAndTimeBasedRollingPolicy.class, PatternLayout.class, RollingFileAppender.class, PatternLayoutEncoder.class},
-                            access = {TypeAccess.PUBLIC_CONSTRUCTORS, PUBLIC_METHODS})
-
-        })
-@AotProxyHints(  { @AotProxyHint(interfaces ={BeanPostProcessor.class, BeanFactoryAware.class, BeanClassLoaderAware.class, ApplicationContextAware.class,
-                    EnvironmentCapable.class, EnvironmentAware.class, Servlet.class, ServletConfig.class, Serializable.class}),
-                    @AotProxyHint(targetClass = DefaultController.class, proxyFeatures = ProxyBits.IS_STATIC),
-                   @AotProxyHint(targetClass= KafkaReceiver.class, proxyFeatures = ProxyBits.IS_STATIC),
-                   @AotProxyHint(targetClass= KafkaSender.class, proxyFeatures = ProxyBits.IS_STATIC) } )
-
+                        access = {TypeAccess.PUBLIC_CONSTRUCTORS, PUBLIC_METHODS}),
+                @TypeHint(types = { ApplicationContextAware.class, Aware.class, MongoOperations.class, IndexOperationsProvider.class, FluentMongoOperations.class, ExecutableFindOperation.class, ExecutableInsertOperation.class, ExecutableUpdateOperation.class, ExecutableRemoveOperation.class, ExecutableAggregationOperation.class, ExecutableMapReduceOperation.class},
+                        access = {TypeAccess.PUBLIC_CONSTRUCTORS, PUBLIC_METHODS, PUBLIC_FIELDS}),
+                @TypeHint(types = { java.util.List.class, java.util.RandomAccess.class, java.lang.Cloneable.class, java.io.Serializable.class, java.util.Collection.class},
+                        access = {TypeAccess.PUBLIC_CONSTRUCTORS, PUBLIC_METHODS, PUBLIC_FIELDS}),
+                @TypeHint(types = { CorrelationScopeDecorator.class, brave.propagation.CurrentTraceContext.ScopeDecorator.class, BraveCurrentTraceContext.class, BaggageManager.class, CurrentTraceContext.class, Tracer.class, TraceContext.class,
+                        java.util.List.class, java.util.RandomAccess.class, java.lang.Cloneable.class, java.io.Serializable.class, java.util.Collection.class},
+                        access = {TypeAccess.PUBLIC_CONSTRUCTORS, PUBLIC_METHODS, PUBLIC_FIELDS})
+        }, jdkProxies = {
+        @JdkProxyHint(types= { BeanPostProcessor.class, BeanFactoryAware.class, BeanClassLoaderAware.class }),
+        @JdkProxyHint(types= { ApplicationContextAware.class, EnvironmentCapable.class, EnvironmentAware.class, Servlet.class, ServletConfig.class, Serializable.class }),
+        @JdkProxyHint(types= { java.util.List.class, java.util.RandomAccess.class, java.lang.Cloneable.class, java.io.Serializable.class, java.util.Collection.class, java.io.Closeable.class}),
+        @JdkProxyHint(types= { ApplicationContextAware.class, Aware.class, MongoOperations.class, IndexOperationsProvider.class, FluentMongoOperations.class, ExecutableFindOperation.class, ExecutableInsertOperation.class, ExecutableUpdateOperation.class, ExecutableRemoveOperation.class, ExecutableAggregationOperation.class, ExecutableMapReduceOperation.class}),
+        @JdkProxyHint(types = { CorrelationScopeDecorator.class, brave.propagation.CurrentTraceContext.ScopeDecorator.class, BraveCurrentTraceContext.class, BaggageManager.class, CurrentTraceContext.class, Tracer.class, TraceContext.class,
+                java.util.List.class, java.util.RandomAccess.class, java.lang.Cloneable.class, java.io.Serializable.class, java.util.Collection.class} )
+}
+)
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class MicroserviceApplication {
 
